64ae56a5bc3e40267d0a523724312957c5c22800
Rectcircle <sunben960729@163.com>
2018-06-07 11:43:31 +0800

    add github_create

--- effective-function.sh
+++ effective-function.sh
@@ -25,3 +25,23 @@ function cpc(){
     grep -v '^\s*$' | grep -v 'cpc' | \
     tail -n $n | xsel -b
 }
+
+# github创建远程仓库
+github_username=rectcircle
+github_api_token=<your github api token> #https://github.com/settings/tokens/new
+
+function github_create() {
+    if [ $1 ]
+    then
+        repo_name=$1
+    else
+        repo_name=`basename $(pwd)`
+        echo "set Repo name to ${repo_name}"
+    fi 
+    curl -u "$github_username:$github_api_token" https://api.github.com/user/repos -d '{"name":"'$repo_name'"}'
+    # git remote add origin git@github.com:username/$repo_name.git
+    git remote add origin  https://github.com/$github_username/$repo_name.git
+}
+
+
+# TODO:gitpush
